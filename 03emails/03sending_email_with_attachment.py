cb9d37dd7c01ba35f8cdd7e2585e71c2dc7f0c80
Freek van den Berg <f@vdberg.us>
2026-09-01 16:12:29 +0200

    update email

--- 03emails/03sending_email_with_attachment.py
+++ 03emails/03sending_email_with_attachment.py
@@ -8,12 +8,23 @@ subject = "This is the subject!"
 attachments = ["tiger.jpeg"]
 
 
-contents = """
+contents1 = """
 Here is the content of the email! 
 Hi!
 """
 
-# sends a single email
+# sends a single email with simple text content and an attachment
 yag = yagmail.SMTP(user=sender, password=password)
-yag.send(to=receiver, subject=subject, contents=contents, attachments=attachments)
+yag.send(to=receiver, subject=subject, contents=contents1, attachments=attachments)
+print("Email Sent!")
+
+contents2 = [
+    "<b>Hello!</b> This email uses <i>rich text</i> formatting.",
+    "<ul><li>Item one</li><li>Item two</li></ul>",
+    '<a href="https://python.org">Visit Python</a>',
+]
+
+# sends a single email with rich text formatting and an attachment
+yag = yagmail.SMTP(user=sender, password=password)
+yag.send(to=receiver, subject=subject, contents=contents2, attachments=attachments)
 print("Email Sent!")
