985c42d91c0de538440e1a100de4656ec8ff0954
Freek van den Berg <f@vdberg.us>
2026-09-01 15:48:30 +0200

    add comment

---
+++ 03emails/04send_emailf_rom_outlook_or_hotmail_address.py
@@ -0,0 +1,15 @@
+import smtplib
+
+sender = "anything@hotmail.com"
+receiver = "f@vdberg.us"
+password = "yourpassword" # your hotmail password
+subject = "This is the subject!"
+
+contents = """
+Here is the content of the email!
+Hi!
+"""
+
+
+
+
