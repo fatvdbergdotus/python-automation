28d432eae3f1719c7c0ca7308a38c544c8758bab
Freek van den Berg <f@vdberg.us>
2026-09-07 15:59:08 +0200

    GUI apps

--- 02_files_and_folders/07delete_files_forever.py
+++ 02_files_and_folders/07delete_files_forever.py
@@ -1,2 +1,17 @@
 from pathlib import Path
 
+root_dir = Path("files_to_delete")
+root_dir.mkdir(exist_ok=True)
+
+# create files in the root dir
+for filename in ["a", "b", "c"]:
+    with open(root_dir / filename, "w") as file:
+        file.write("empty")
+        print("created " + str(root_dir / filename))
+
+# permanently delete all files in the root_dir
+for path in root_dir.glob("*"):
+    with open(path, "wb") as file:
+        file.write(b'')
+    path.unlink()
+    print("deleted " + str(path))
---
+++ 07web_apps_and_desktop_gui_apps/04file_destroyer_gui_pyqt6.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+from PyQt6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget, QPushButton, QFileDialog
+from PyQt6.QtCore import Qt
+
+def select_files():
+    window.filenames, _ = QFileDialog.getOpenFileNames(window, "Select files")
+    filenames_label.setText("\n".join(window.filenames))
+    delete_button.setEnabled(True)
+
+def delete_files():
+    for filename in window.filenames:
+        path = Path(filename)
+# uncomment the three lines below to really delete the files
+#       with open(path, "wb") as file:
+#           file.write(b'')
+#       path.unlink()
+        print("deleted " + str(path))   
+    filenames_label.setText("Deletion successful")
+    window.filenames=[]        
+    delete_button.setEnabled(False)
+
+app = QApplication([])
+
+window = QWidget()
+window.setWindowTitle("File Destroyer")
+
+layout = QVBoxLayout()
+
+window.setLayout(layout)
+
+window.filenames = []
+
+prompt_label = QLabel("Select the files you want to destroy. The files will be <font color='red'>deleted</font>.")
+layout.addWidget(prompt_label)
+
+open_button = QPushButton("Select files")
+open_button.setToolTip("Select the files to open.")
+open_button.setFixedWidth(120)
+layout.addWidget(open_button, alignment=Qt.AlignmentFlag.AlignCenter)
+open_button.clicked.connect(select_files)
+
+delete_button = QPushButton("Delete files")
+delete_button.setToolTip("Delete the selected files.")
+delete_button.setFixedWidth(120)
+layout.addWidget(delete_button, alignment=Qt.AlignmentFlag.AlignCenter)
+delete_button.clicked.connect(delete_files)
+delete_button.setEnabled(False)
+
+filenames_label = QLabel("")
+layout.addWidget(filenames_label)
+
+window.show()
+app.exec()
+
